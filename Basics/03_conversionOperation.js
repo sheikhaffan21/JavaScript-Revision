b48d1ed086401ed5de8c38adc11abc429eebab4d
Affan Ahmed Sheikh Siddique <sa7709394@gmail.com>
2024-04-05 18:20:32 +0000

    Changes

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = false
+
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//Number() =>
+
+// "33" => 33
+// "33abc" => NaN  //NaN is of number Type
+// null => 0
+//undefined => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "affan";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+//Boolean() =>
+
+//1 => true
+//0 => false
+//"" => false
+//"Affan" => true
+
+let someNumber =33
+let StringNumber = String(someNumber)
+console.log(typeof(StringNumber))
+console.log(StringNumber)
