0452048082bcc4625d34d5d8959d77006da9a5a7
Affan Ahmed Sheikh Siddique <sa7709394@gmail.com>
2024-04-06 18:37:28 +0000

    Changes

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -30,3 +30,53 @@ let someNumber = 33
 let StringNumber = String(someNumber)
 console.log(typeof(StringNumber))
 console.log(StringNumber)
+
+//string() =>
+//anything in string format
+
+/* OPERATIONS */
+
+//negative value
+let value =3;
+let negValue = -value;
+console.log(negValue);
+
+//basic operator
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+//concatenation
+let str1="hello"
+let str2=" affan"
+let str3 = str1+str2
+console.log(str1+str2)
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2) //122 beacuse if string come first then in concatenation it will consider as string
+console.log(1+2+"2") //32 because numbers comes first
+
+console.log(3+4*5%3) //not good practice
+
+console.log(true) //prints true
+console.log(+true) //prints 1 => conversion to number due to increment opertor
+
+console.log(+"") //prints 0 
+
+let num1, num2, num3
+num1 = num2 = mum3 = 2+2 //not good practice
+
+
+/*Increment Decrement Operator*/
+let gameCounter =100
+++gameCounter //prefix increment
+gameCounter++ //postfix increment
+
+
+
+
